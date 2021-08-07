4ea0dd2fef2a3240cf57d27c108323be12cfc56d
Mahdi Samadani <samadani.mahdi@gmail.com>
2021-08-07 21:02:26 +0430

    chore: config tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import './styles/tailwind.css'
 import Home from './pages/Home'
 
 ReactDOM.render(
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const App: React.FunctionComponent<unknown> = () => {
 	return (
-		<div className="App">
-			<p>Simple Todo List</p>
+		<div className={'pt-10'}>
+			<div className={'bg-blue-100 max-w-2xl mx-auto p-8 md:p-12 rounded-lg shadow-xl'}>
+				Simple todo app
+			</div>
 		</div>
 	)
 }

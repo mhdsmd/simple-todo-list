cfc3519d92ac404143ef47f63578bdecc5d08e45
Mahdi Samadani <samadani.mahdi@gmail.com>
2021-08-07 22:56:23 +0430

    feat: add borderless input instead of label in NodeItem

--- src/components/NodeItem/index.tsx
+++ src/components/NodeItem/index.tsx
@@ -6,14 +6,16 @@ type NodeItemProps = {
 }
 
 const NodeList: React.FunctionComponent<NodeItemProps> = (props: NodeItemProps) => {
+	const [value, setValue] = React.useState<string>(props.item.title)
+
 	return (
 		<div className={'flex items-center text-gray-700'}>
 			{/* Black circle */}
 			<span className={'text-xs pr-2'}>{'\u2B24'}</span>
 
 			{/* Node title */}
-			<span className={'text-base'}>
-				{props.item.title}
+			<span className={'text-base w-full'}>
+				<input className={'w-full nodeInput'} value={value} onChange={e => setValue(e.target.value)} />
 			</span>
 		</div>
 	)

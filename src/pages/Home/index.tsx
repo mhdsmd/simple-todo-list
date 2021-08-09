d05a5e487bcd73a02ffb0c28a5a662ec8aca8506
Mahdi Samadani <samadani.mahdi@gmail.com>
2021-08-09 19:40:45 +0430

    feat: implement caching nodes to localStorage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,15 +2,27 @@ import React from 'react'
 import NodeList from '../../components/NodeList'
 import { NodeListExample } from '../../mock'
 import { NodeListType } from '../../interfaces'
-import { modifyNodeWithId, storeNodeListToLocalStorage } from '../../utils'
+import { modifyNodeWithId, storeNodeListToLocalStorage, getNodeListFromLocalStorage } from '../../utils'
 
 const App: React.FunctionComponent<unknown> = () => {
-	const [mockData, setMockData] = React.useState<NodeListType>(NodeListExample)
+	const [nodes, setNodes] = React.useState<NodeListType>([])
+
+	// Load application lifeCycle
+	React.useEffect(() => {
+		const initialNodes = getNodeListFromLocalStorage()
+		if (initialNodes.length) {
+			// Use cached nodes
+			setNodes(initialNodes)
+		} else {
+			// Use simple data for first time load
+			setNodes(NodeListExample)
+		}
+	}, [])
 
 	const handleOnChange = (value: string, id: string | number) => {
 		try {
-			const _modifiedNodeList = modifyNodeWithId(id, value, mockData)
-			setMockData(_modifiedNodeList)
+			const _modifiedNodeList = modifyNodeWithId(id, value, nodes)
+			setNodes(_modifiedNodeList)
 			storeNodeListToLocalStorage(_modifiedNodeList)
 		} catch (e) {
 			console.log(e)
@@ -20,7 +32,7 @@ const App: React.FunctionComponent<unknown> = () => {
 	return (
 		<div className={'pt-10'}>
 			<div className={'bg-blue-100 max-w-2xl mx-auto p-8 md:p-12 rounded-lg shadow-xl'}>
-				<NodeList data={mockData} onChange={handleOnChange} />
+				<NodeList data={nodes} onChange={handleOnChange} />
 			</div>
 		</div>
 	)
